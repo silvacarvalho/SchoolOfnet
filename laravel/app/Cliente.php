1729615896ea0bfc186cd7fb8fa8d0c9976f3832
Rafael Carvalho <rafael.carvalho@impix.com.br>
2015-10-12 01:06:58 -0300

    Corrigindo nome da propriedade para $fillable.

--- laravel/app/Cliente.php
+++ laravel/app/Cliente.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    protected $filltable = [
+    protected $fillable = [
         'nome',
         'responsavel',
         'email',
